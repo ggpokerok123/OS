af0c4290cb723edcf5f26beeecebb71a38ca40b6
vlano003 <85347360+vlano003@users.noreply.github.com>
2022-12-22 09:18:04 +0200

    lab 3 full

---
+++ lab3/src/commm/Instruction.java
@@ -0,0 +1,12 @@
+package commm;
+
+public class Instruction {
+    public String inst;
+    public long addr;
+
+    public Instruction(String inst, long addr) {
+        this.inst = inst;
+        this.addr = addr;
+    }
+
+}
---
+++ lab3/src/commm/Page.java
@@ -0,0 +1,27 @@
+package commm;
+
+
+public class Page {
+    public int id;
+    public int physical;
+    public byte R;
+    public byte M;
+    public int inMemTime;
+    public int lastTouchTime;
+    public long high;
+    public long low;
+    public boolean lastUsedInReplacement;
+
+    public Page(int id, int physical, byte R, byte M, int inMemTime, int lastTouchTime, long high, long low, boolean lastUsedInReplacement) {
+        this.id = id;
+        this.physical = physical;
+        this.R = R;
+        this.M = M;
+        this.inMemTime = inMemTime;
+        this.lastTouchTime = lastTouchTime;
+        this.high = high;
+        this.low = low;
+        this.lastUsedInReplacement = lastUsedInReplacement;
+    }
+
+}
